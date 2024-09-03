6f93ccd26c74235f0ee907da4c26cde0e938078c
Ebraheem <ebraheem20012020@gmail.com>
2024-09-03 12:06:00 +0300

    service extensions

--- Pharmacy.UI/Program.cs
+++ Pharmacy.UI/Program.cs
@@ -1,10 +1,10 @@
+using Pharmacy.UI.Utilities;
 using Pharmacy.UI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.Configure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Pharmacy.UI/Utilities/ServiceExtensions.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Services;
+using Pharmacy.Domain.Models;
+using Pharmacy.Domain.Interfaces;
+using Pharmacy.Application.Utilities;
+using Pharmacy.Application.Services;
+
+namespace Pharmacy.UI.Utilities;
+
+
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection Configure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+        services.AddMudServices();
+        services.AddServices();
+        services.AddClient(configuration);
+        return services;
+    }
+
+    public static IServiceCollection AddClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        Uri BaseAddress = new(configuration.GetSection("BaseAddress").Value!);
+        services.AddHttpClient<IService<User>, UserService>(client => client.BaseAddress = BaseAddress);
+        services.AddHttpClient<IService<Product>, ProductService>(client => client.BaseAddress = BaseAddress);
+        services.AddHttpClient<IService<ProductProvider>, GenericService<ProductProvider>>(client => client.BaseAddress = BaseAddress);
+        services.AddHttpClient<IService<ScarceProduct>, GenericService<ScarceProduct>>(client => client.BaseAddress = BaseAddress);
+        services.AddHttpClient<IService<IncomingOrder>, GenericService<IncomingOrder>>(client => client.BaseAddress = BaseAddress);
+        services.AddHttpClient<IService<Customer>, GenericService<Customer>>(client => client.BaseAddress = BaseAddress);
+        services.AddHttpClient<IService<Order>, GenericService<Order>>(client => client.BaseAddress = BaseAddress);
+        return services;
+    }
+}
