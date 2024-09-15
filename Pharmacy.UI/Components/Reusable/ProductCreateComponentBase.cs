e6e24934a0f04556e1a106db747159d489462f9e
Ebraheem <ebraheem20012020@gmail.com>
2024-09-15 06:19:20 +0300

    product create component

---
+++ Pharmacy.UI/Components/Reusable/ProductCreateComponentBase.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Pharmacy.Domain.Interfaces;
+using Pharmacy.Domain.Models;
+using MudBlazor;
+
+namespace Pharmacy.UI.Components.Reusable;
+
+
+public class ProductCreateComponentBase : ComponentBase
+{
+    [CascadingParameter]
+    private MudDialogInstance? MudDialog { get; set; }
+
+    [Inject]
+    private IRepositoryManager? _manager {get; set;}
+
+    [SupplyParameterFromForm]
+    protected ProductCreate productCreate {get; set;} = new();
+
+    [Parameter]
+    public EventCallback<Product> OnProductCreated { get; set; }
+
+    protected void CloseDialog() =>
+        MudDialog!.Close(DialogResult.Ok(true));
+
+    protected void CancelDialog() =>
+        MudDialog!.Cancel();
+
+    protected async void OnValidSubmit(EditContext context)
+    {
+        Product? product = await _manager!.Products.Create(productCreate);
+        await OnProductCreated.InvokeAsync(product);
+        StateHasChanged();
+    }
+}
